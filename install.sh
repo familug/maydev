dc030400b585fa2e1a07e655c1b7505b1b1d2868
HVN <hvn@familug.org>
2025-10-29 16:14:48 +0000

    Gen key if not exists and ssh localhost

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+set -x
 sudo apt install -y git curl
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+if [ -f ~/.ssh/id_ed25519 ]; then
+    exit 0
+else
+    ssh-keygen -q -N "" -f ~/.ssh/id_ed25519
+    cat ~/.ssh/id_ed25519.pub >> ~/.ssh/authorized_keys
+    ssh -oStrictHostKeyChecking=no localhost uptime
+fi
